5b3d4f23ec64533699e568b9d78d0ef3a1a81b5a
DockoVH <veljko.doca@elfak.rs>
2024-06-25 01:32:38 +0200

    Ispravljene greške.

--- Projekat 3/OracleWebAPI/OracleWebAPI/Client 2024/main.js
+++
@@ -1,4 +0,0 @@
-import { Prodavnica } from "./prodavnica.js";
-
-const prodavnica = new Prodavnica("https://localhost:7234/Prodavnica/PreuzmiProdavnice", document.body);
-await prodavnica.loadData();
